4b2087ec4e615a4d9ddf763fe40a113e6d2e9024
Vijay Stroup <vijaystroup@gmail.com>
2021-09-05 18:27:06 -0400

    select menu interaction

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,18 +1,32 @@
 const InteractionCreate = {
   name: 'interactionCreate',
   async execute(interaction) {
-    if (!interaction.isCommand()) return
+    // command
+    if (interaction.isCommand()) {
+      const command = interaction.client.commands.get(interaction.commandName)
+      if (!command) return
 
-    const command = interaction.client.commands.get(interaction.commandName)
-    if (!command) return
+      try {
+        await command.execute(interaction)
+      } catch (error) {
+        console.error(error)
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
+      }
+    }
 
-    try {
-      await command.execute(interaction)
-    } catch (error) {
-      console.error(error)
-      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
+    // select menu
+    if (interaction.isSelectMenu()) {
+      const command = interaction.client.commands.get(interaction.message.interaction.commandName)
+      if (!command) return
+
+      try {
+        await command.onSelect(interaction)
+      } catch (error) {
+        console.error(error)
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
+      }
     }
-    }
+  }
 }
 
 export default InteractionCreate
