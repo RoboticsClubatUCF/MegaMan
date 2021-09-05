7e055944fe41c6f187c0e7a8c377203dfcb1cc6b
Vijay Stroup <vijaystroup@gmail.com>
2021-09-05 16:32:35 -0400

    register commands

---
+++ deploy-commands.js
@@ -0,0 +1,27 @@
+import fs from 'fs'
+import { config } from 'dotenv'
+import { REST } from '@discordjs/rest'
+import { Routes } from 'discord-api-types/v9'
+
+config()
+
+const commands = []
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+for (const file of commandFiles) {
+  const command = await import(`./commands/${file}`)
+	commands.push(command.default.data.toJSON())
+}
+
+(async () => {
+  const rest = new REST({ version: '9' }).setToken(process.env.TOKEN)
+  try {
+    await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      { body: commands.default },
+    )
+
+    console.log('Successfully registered application commands.')
+  } catch (error) {
+    console.error(error)
+  }
+})()
