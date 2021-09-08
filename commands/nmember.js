a521609860ab219800f734c4a0db796f6670faaf
lntel <joeharris461767@gmail.com>
2021-09-08 17:12:12 +0100

    Fixes #12

---
+++ commands/nmember.js
@@ -0,0 +1,20 @@
+import { SlashCommandBuilder } from '@discordjs/builders'
+
+const NMember = {
+  builder: new SlashCommandBuilder()
+    .setName('nmember')
+    .setDescription('Returns the number of members on the server!'),
+  async execute(interaction) {
+
+    const role = interaction.guild.roles.cache.find(role => role.name === 'Members');
+
+    if(!role)
+        return await interaction.reply('The members role does not exist');
+
+    const total = role.members.size;
+
+    await interaction.reply(`There are currently ${total} members!`);
+  }
+}
+
+export default NMember
