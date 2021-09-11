a1d69c36ebf7d4384e337cb9796821306b0710d2
Vijay Stroup <vijaystroup@gmail.com>
2021-09-11 15:55:13 -0400

    linting and naming fixes

--- commands/nmember.js
+++ commands/nmember.js
@@ -1,20 +1,19 @@
 import { SlashCommandBuilder } from '@discordjs/builders'
 
-const NMember = {
+const NMembers = {
   builder: new SlashCommandBuilder()
-    .setName('nmember')
-    .setDescription('Returns the number of members on the server!'),
+    .setName('nmembers')
+    .setDescription('Returns the number of members on the server.'),
   async execute(interaction) {
-
-    const role = interaction.guild.roles.cache.find(role => role.name === 'Members');
+    const role = interaction.guild.roles.cache.find(role => role.name === 'Members')
 
     if(!role)
-        return await interaction.reply('The members role does not exist');
+      return await interaction.reply('The members role does not exist')
 
-    const total = role.members.size;
+    const total = role.members.size
 
-    await interaction.reply(`There are currently ${total} members!`);
+    await interaction.reply(`There are currently ${total} members!`)
   }
 }
 
-export default NMember
+export default NMembers
