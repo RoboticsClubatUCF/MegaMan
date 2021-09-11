d4fbcb9049cf31f23d97e765ef5ad30918d496a4
Vijay Stroup <vijaystroup@gmail.com>
2021-09-11 00:21:32 -0400

    setup guild permissions onready

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,50 @@
+import fs from 'fs'
+import { config } from 'dotenv'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+config()
+
+const getCommands = async () => {
+  const commands = {}
+  const commandFiles = fs.readdirSync(`${__dirname}/../commands`).filter(file => file.endsWith('.js'))
+  for (const file of commandFiles) {
+    const command = await import(`${__dirname}/../commands/${file}`)
+    commands[command.default.builder.name] = command.default
+  }
+  return commands
+}
+
 const Ready = {
   name: 'ready',
   once: true,
-  execute(client) {
+  async execute(client) {
+    if (!client.application?.owner) await client.application?.fetch()
+
+    const guild = await client.guilds.cache.get(process.env.GUILD_ID)
+
+    const commands = await getCommands()
+    const guildCommands = await guild.commands.fetch()
+    const guildRoles = await guild.roles.cache
+
+    for (const cmd of guildCommands) {
+      if (commands[cmd[1].name].roles) {
+        const permissions = []
+        for (const r of commands[cmd[1].name].roles) {
+          const role = await guildRoles.find(role => role.name === r)
+          permissions.push({
+            id: role.id,
+            type: 'ROLE',
+            permission: true
+          })
+        }
+
+        await cmd[1].permissions.add({ permissions });
+      }
+    }
+
     console.log(`Ready! Logged in as ${client.user.tag}`)
   }
 }
