cf10bfac2fc7fe0a6e8e72c3e72cf64d8724039d
Vijay Stroup <vijaystroup@gmail.com>
2021-09-11 18:21:21 -0400

    dues embed

--- commands/dues.js
+++ commands/dues.js
@@ -1,18 +1,28 @@
 import { SlashCommandBuilder } from '@discordjs/builders'
+import { MessageEmbed } from 'discord.js'
+
+const url = 'https://www.paypal.com/paypalme/rcucf'
+
+const paymentEmbed = new MessageEmbed()
+  .setColor('#FEC904')
+  .setURL(url)
+  .setAuthor('PayPal', 'https://i.imgur.com/Yg5ZlFc.png', url)
+  .setDescription('Dues are to be paid through our PayPal.  All proceeds are used to fund our projects and lab space.')
+  .setThumbnail('https://i.imgur.com/udziL5c.png')
+  .addFields(
+    { name: 'Payment Link', value: url },
+    { name: 'Amount', value: '$20.00', inline: true },
+    { name: 'Term', value: 'Fall 2021', inline: true }
+  )
+  .setTimestamp()
 
 const Dues = {
   builder: new SlashCommandBuilder()
     .setName('dues')
-    .setDescription('Respond with a link to PayPal to pay dues'),
+    .setDescription('Respond with a link to PayPal to pay dues.'),
+  channels: ['bot-cmds'],
   async execute(interaction) {
-      
-    // TODO add relevant paypal url
-    // 7 seconds? too long? too short?
-    await interaction.reply({
-        content: 'http://paypal.com',
-        ephemeral: true
-    });
-
+    await interaction.reply({ embeds: [paymentEmbed] })
   }
 }
 
