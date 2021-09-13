33de44a3b6f22cc65d1ce103587b092786cb35ed
Vijay Stroup <vijaystroup@gmail.com>
2021-09-12 20:47:43 -0400

    labTimes command

---
+++ commands/labTimes.js
@@ -0,0 +1,40 @@
+import { SlashCommandBuilder } from '@discordjs/builders'
+import { MessageEmbed } from 'discord.js'
+
+const labTimesEmbed = new MessageEmbed()
+  .setColor('#2ECC71')
+  .setTitle('Lab Times')
+  .setDescription('Below you can find when our lab will be open. Officers specialties are listed as well ' + 
+    'but that is not the only activites that are allowed on those days. You are allowed to go to the lab ' +
+    'when ever it is open and work on any project you\'d like.')
+  .setThumbnail('https://i.imgur.com/udziL5c.png')
+  .addFields(
+    { name: 'Datetime', value: 'Monday 2:30-6:30pm', inline: true },
+    { name: 'Officer', value: 'Alex', inline: true },
+    { name: 'Specialty', value: 'Software', inline: true },
+
+    { name: 'Datetime', value: 'Tuesday 5:00-9:00pm', inline: true },
+    { name: 'Officer', value: 'Wes', inline: true },
+    { name: 'Specialty', value: 'Computer ENG/ROS/SIM', inline: true },
+
+    { name: 'Datetime', value: 'Wednesday 4:30-9:00pm', inline: true },
+    { name: 'Officer', value: 'Dwight', inline: true },
+    { name: 'Specialty', value: 'Mech ENG', inline: true },
+
+    { name: 'Datetime', value: 'Thursday 5:00-9:00pm', inline: true },
+    { name: 'Officer', value: 'Marc', inline: true },
+    { name: 'Specialty', value: 'Computer ENG/ROS/Mech ENG', inline: true },
+  )
+  .setTimestamp()
+
+const LabTimes = {
+  builder: new SlashCommandBuilder()
+    .setName('labtimes')
+    .setDescription('Respond with open lab times.'),
+  channels: ['bot-cmds'],
+  async execute(interaction) {
+    await interaction.reply({ embeds: [labTimesEmbed] })
+  }
+}
+
+export default LabTimes
