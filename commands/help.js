ac3a9870fe9dbc4e149a3604f7c58514cb0d25e2
Vijay Stroup <vijaystroup@gmail.com>
2021-10-28 14:37:10 -0400

    quick fix help command

--- commands/help.js
+++ commands/help.js
@@ -2,12 +2,6 @@ import { SlashCommandBuilder } from '@discordjs/builders'
 import { MessageEmbed } from 'discord.js'
 import getCommands from '../utils/getCommands.js'
 
-const commandsEmbed = new MessageEmbed()
-  .setColor('#649CDB')
-  .setTitle('Commands')
-  .setDescription('Below is a list of all available commands and their descriptions.\n*Members only command\n**Officer only command')
-  .setThumbnail('https://i.imgur.com/udziL5c.png')
-  .setTimestamp()
 
 let commands
 (async () => { commands = await getCommands() })()
@@ -18,6 +12,12 @@ const Help = {
     .setDescription('Respond with a list of commands.'),
   channels: ['bot-cmds'],
   async execute(interaction) {
+    const commandsEmbed = new MessageEmbed()
+      .setColor('#649CDB')
+      .setTitle('Commands')
+      .setDescription('Below is a list of all available commands and their descriptions.\n*Members only command\n**Officer only command')
+      .setThumbnail('https://i.imgur.com/udziL5c.png')
+      .setTimestamp()
     for (const key in commands) {
       if (commands[key].roles && commands[key].roles.includes('Members'))
         commandsEmbed.addField(key, `${commands[key].builder.description}*`)
