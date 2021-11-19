f1e5f71f8847448c2add3fd28a2e6ec36d36d4d4
Vijay Stroup <vijaystroup@gmail.com>
2021-11-19 17:37:50 -0500

    anti spam event

---
+++ events/antiSpam.js
@@ -0,0 +1,31 @@
+const memberMessages = {}
+
+const AntiSpam = {
+  name: 'messageCreate',
+  async execute(message) {
+    const mId = message.author.id
+
+    if (mId in memberMessages) {
+      if (memberMessages[mId].lastMessage === message.content) {
+        if (memberMessages[mId].repetition >= 2) {
+          await message.delete()
+          await message.author.send('You have sent the same message too frequently, please do not spam.')
+          const officersChannel = await message.guild.channels.cache.find(c => c.name === 'officers')
+          await officersChannel.send(`<@${mId}> warned for spamming message \`\`\`${message.content}\`\`\``)
+        } else memberMessages[mId].repetition++
+      } else {
+        memberMessages[mId] = {
+          lastMessage: message.content,
+          repetition: 1
+        }
+      }
+    } else {
+      memberMessages[mId] = {
+        lastMessage: message.content,
+        repetition: 1
+      }
+    }
+  }
+}
+
+export default AntiSpam
