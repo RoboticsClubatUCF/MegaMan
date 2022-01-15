18b404ce6ac8f12166dff0888dfc64b3a8a53600
Vijay Stroup <vijaystroup@gmail.com>
2022-01-14 19:13:35 -0500

    labCheckin command for officers

---
+++ commands/labCheckin.js
@@ -0,0 +1,30 @@
+import { SlashCommandBuilder } from '@discordjs/builders'
+import status from '../utils/labStatus.js'
+
+const LabCheckin = {
+  builder: new SlashCommandBuilder()
+    .setName('labcheckin')
+    .setDescription('checkin to the lab.')
+    .addIntegerOption(option => 
+      option.setName('close')
+        .setDescription('Hour you are closing in 24hr time.')
+        .setRequired(true)
+    ),
+  channels: ['officers'],
+  roles: ['Officers'],
+  async execute(interaction) {
+    const close = interaction.options.getInteger('close')
+
+    if (close < 0 || close > 23) {
+      await interaction.reply({ content: 'Hour must be between 0 and 23.', ephemeral: true })
+      return
+    }
+
+    status.data.push({
+      id: interaction.user.id,
+      close: interaction.options.getInteger('close')
+    })
+  }
+}
+
+export default LabCheckin
