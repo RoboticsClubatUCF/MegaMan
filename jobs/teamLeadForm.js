db38d36135b0da3e282c93ac080f9c764fd5816e
Vijay Stroup <vijaystroup@gmail.com>
2022-10-13 20:31:31 -0400

    remove trying to mention role

--- jobs/teamLeadForm.js
+++ jobs/teamLeadForm.js
@@ -4,8 +4,7 @@ const teamLeadForm = {
   cronPattern: '0 20 * * 4', // every thursday at 20:00
   channel: 'teamleads',
   async execute(channel) {
-    const role = channel.guild.roles.cache.find(r => r.name === 'Team Leads')
-    await channel.send(`<@${role.id}>, if you have not already, please fill out this form regarding this week's project meeting. The information obtained from this form allows us to find and solve issues within teams, offer help where needed, as well as establish trends that aid in future project development.\n${googleDoc}`)
+    await channel.send(`Team Leads, if you have not already, please fill out this form regarding this week's project meeting. The information obtained from this form allows us to find and solve issues within teams, offer help where needed, as well as establish trends that aid in future project development.\n${googleDoc}`)
   }
 }
 
