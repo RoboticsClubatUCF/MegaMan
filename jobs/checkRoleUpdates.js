d94e812563ea4099843e2ec7b2461ab1a2c4d965
BiggiePete <pter.cross222@gmail.com>
2024-02-08 20:12:10 -0500

    formatting

--- jobs/checkRoleUpdates.js
+++ jobs/checkRoleUpdates.js
@@ -19,39 +19,33 @@ const checkForRoleUpdates = {
     /*
     Data should come in as a string[]
     */
+    // users is an array containing every person who has paid dues on the website
     console.log(users);
 
-    const guilds = await client.guilds.fetch();
-    for (const g of guilds) {
-      if (g[1].id === "267370501280759810") {
-        const guild = await g[1].fetch();
-        const role = await guild.roles.fetch("267373066290593794");
-        const members = await guild.members.fetch();
-        for (const m of members) {
-          if (
-            users.includes(m[1].user.username) ||
-            users.includes(m[1].nickname) ||
-            users.includes(m[1].displayName)
-          ) {
-            if (
-              !m[1].roles
-                .valueOf()
-                .find(async (r) => r.id === "267373066290593794")
-            ) {
-              await m[1].roles.add(role);
-            }
-          } else {
-            try {
-              // only try to remove a role if they have it
-              m[1].roles.valueOf().find(async (r) => {
-                if (r.id == "267373066290593794") {
-                  await m[1].roles.remove(role);
-                  return true;
-                }
-              });
-            } catch (ex) {}
-          }
+    const guild = await client.guilds.fetch("267370501280759810");
+    const role = await guild.roles.fetch("267373066290593794");
+    const members = await guild.members.fetch();
+    for (const m of members) {
+      if (
+        users.includes(m[1].user.username) ||
+        users.includes(m[1].nickname) ||
+        users.includes(m[1].displayName)
+      ) {
+        if (
+          !m[1].roles.valueOf().find(async (r) => r.id === "267373066290593794")
+        ) {
+          await m[1].roles.add(role);
         }
+      } else {
+        try {
+          // only try to remove a role if they have it
+          m[1].roles.valueOf().find(async (r) => {
+            if (r.id == "267373066290593794") {
+              await m[1].roles.remove(role);
+              return true;
+            }
+          });
+        } catch (ex) {}
       }
     }
   },
