977b9cb01bdc590feea12381322c83a1baed021f
Dwight Howard, II <104738523+Dwight-2@users.noreply.github.com>
2024-02-13 23:29:44 -0500

    team lead message

--- jobs/teamLeadForm.js
+++ jobs/teamLeadForm.js
@@ -1,6 +1,6 @@
-/*const googleDoc = "https://forms.gle/R6MGxEWSGqVEYxdF9";
+const googleDoc = "https://forms.gle/R6MGxEWSGqVEYxdF9";
 
-const teamLeadForm = {
+/*const teamLeadForm = {
   cronPattern: "0 20 * * 4", // every thursday at 20:00
   channel: "teamleads",
   async execute(channel) {
@@ -8,7 +8,6 @@ const teamLeadForm = {
       `Team Leads, if you have not already, please fill out this form regarding this week's project meeting. The information obtained from this form allows us to find and solve issues within teams, offer help where needed, as well as establish trends that aid in future project development.\n${googleDoc}`,
     );
   },
-};
+};*/
 
 export default teamLeadForm;
-*/
